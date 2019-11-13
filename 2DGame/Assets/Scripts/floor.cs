f95700782058f3dbf4b7648b6103469a9d7b1776
kaihong870122 <57181273+kaihong870122@users.noreply.github.com>
2019-11-13 21:38:57 +0800

    地板移動
    Transform教學

--- 2DGame/Assets/Scripts/floor.cs
+++ 2DGame/Assets/Scripts/floor.cs
@@ -2,6 +2,12 @@
 
 public class floor : MonoBehaviour
 {
-[Header("速度"), Range(0.1f, 10f)]
-public float Ground = 1f;
+    [Header("速度"), Range(-0.1f, 10f)]
+    public float Ground = 1f;
+
+    public Transform ground;
+    private void Update()
+    {
+        ground.Translate(-0.05f,0,0);
+    }
 }
