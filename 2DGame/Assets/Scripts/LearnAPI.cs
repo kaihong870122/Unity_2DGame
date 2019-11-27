2eebbb8a46e2d705c335d7fba7867a91d972a3ad
kaihong870122 <57181273+kaihong870122@users.noreply.github.com>
2019-11-27 21:33:19 +0800

    小雞&水管程式

--- 2DGame/Assets/Scripts/LearnAPI.cs
+++ 2DGame/Assets/Scripts/LearnAPI.cs
@@ -2,5 +2,28 @@
 
 public class LearnAPI : MonoBehaviour
 {
+    public int speed = 0;
+    //非靜態需要先定義欄位，並且在屬性面板上套用對應物件
+    public Transform bird;
+    public Transform ground;
 
+    private void Start()
+    {
+        Debug.Log("一般");
+        Debug.LogWarning("警告");
+        Debug.LogError("錯誤");
+        Debug.Log(Mathf.PI);
+        //靜態屬性 static properties
+        Debug.Log(Random.value);
+        //靜態方法 static metgod
+        Debug.Log(Random.Range(1,10));
+        //非靜態屬性用法 properties
+        Debug.Log(bird.position);
+        Debug.Log(ground.position);
+    }
+    private void Update()
+    {
+        print(Input.GetKeyDown("mouse 0"));
+        print(Input.GetKeyDown(KeyCode.Mouse1));
+    }
 }
--- 2DGame/Assets/Scripts/chicken.cs
+++ 2DGame/Assets/Scripts/chicken.cs
@@ -2,24 +2,56 @@
 
 public class chicken : MonoBehaviour
 {
-    [Header("跳高"), Range(-10, 10)]
+    [Header("跳高"), Range(100, 1000)]
     public int jumpHight = 0;
-    [Header("死亡"),Tooltip("你已死了")]
+    [Header("旋轉"), Range(10,100)]
+    public float angle = 50f;
+    [Header("死亡"), Tooltip("你已死了")]
     public bool isdead;
+
+    public GameObject goScore, goGM;
+    public Rigidbody2D r2d;   //2D剛體
 
     /// <summary>
     /// 小雞跳躍
     /// </summary>
     private void Jump()
     {
-        
+        if (isdead) return; //如果已死亡 勾選就跳出
+     
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            goScore.SetActive(true);
+            goGM.SetActive(true);
+
+            r2d.Sleep();
+            r2d.gravityScale = 2;   //地心引力 = 1
+            r2d.AddForce(new Vector2(0, jumpHight));  //推力
+        }
+        r2d.SetRotation(angle * r2d.velocity.y);
     }
+    private void Update()
+    {
+        Jump();
+    }
+    //碰撞開始事件：物件碰撞開始時執行一次(紀路碰撞物件資訊)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //print(collision.gameObject.name);  //碰撞,遊戲物件物件.名稱
+        Dead();
+    }
+    //觸發開始事件：物件觸發開始時執行一次(紀錄狀物件資訊),針對有勾選 Is Trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Dead();
+    }
+
     /// <summary>
     /// 小雞死亡
     /// </summary>
     private void Dead()
     {
-
+        isdead = true;
     }
     /// <summary>
     /// 通過水管
